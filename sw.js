b98650edf5ea2747069013d62c1d32c73beac1c1
Shivraj Jadeja <shivraj27jadeja@gmail.com>
2025-09-11 18:51:55 -0700

    Added Delete Feature

--- sw.js
+++ sw.js
@@ -1,36 +1,39 @@
-const CACHE = 'homedesk-lite-v1';
+const CACHE = 'homedesk-lite-v2'; // bump version so old cache is cleared
 const ASSETS = [
-'./',
-'./index.html',
-'./manifest.json',
-'./sw.js',
-'./vis-network.min.js',
-'./icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './vis-network.min.js',
+  './icon-512.png'
 ];
 
-
 self.addEventListener('install', (e) => {
-e.waitUntil(
-caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
-);
-self.skipWaiting();
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
 });
-
 
 self.addEventListener('activate', (e) => {
-e.waitUntil(
-caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-);
-self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
-
-// Offline-first for app shell
+// Network-first strategy
 self.addEventListener('fetch', (e) => {
-const url = new URL(e.request.url);
-if (url.origin === location.origin) {
-e.respondWith(
-caches.match(e.request).then((res) => res || fetch(e.request))
-);
-}
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        // Update cache in background
+        const resClone = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request)) // fallback to cache if offline
+  );
 });
